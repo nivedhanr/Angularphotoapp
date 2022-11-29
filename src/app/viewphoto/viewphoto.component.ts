147d23e6650c8c506fc138f612fd7bd3a6826feb
nivedhanr <nivedharenahan@gmail.com>
2022-11-29 15:46:42 +0530

    service

--- src/app/viewphoto/viewphoto.component.ts
+++ src/app/viewphoto/viewphoto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-viewphoto',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./viewphoto.component.css']
 })
 export class ViewphotoComponent {
+  constructor(private api:ApiService){
+    api.fetchphoto().subscribe(
+      (response)=>{
+        this.data=response;
+      }
+    )
+  }
   data:any=[]
 }
